c5d8d45362b65514f1fdda3cd4db11a31dce7271
Floyd Kretschmar <floyd.kretschmar@otto.de>
2024-07-24 14:23:28 +0200

    [bump-major] Release version 1.0

--- fixturize-core/build/resources/test/fixtures/CustomValueProviderClassFixture.java
+++
@@ -1,8 +0,0 @@
-package de.floydkretschmar.fixturize.mocks;
-
-public class CustomValueProviderClassFixture {
-    public static java.lang.String STRING_FIELD = "stringFieldValue";
-    public static int INT_FIELD = 10;
-
-
-}
--- fixturize-playground/build/generated/sources/annotationProcessor/java/main/TestFixture.java
+++
@@ -1,6 +0,0 @@
-public class TestFixture {
-    public static int FIELD = 0;
-    public static java.lang.Boolean OTHER_FIELD = false;
-
-
-}
